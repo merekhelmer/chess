fc20201d569ce8bfe5265ceafcb6dbbcbfcfdb14
Merek Helmer <merek.helmer@byu.edu>
2024-09-19 11:41:35 -0600

    Added KnightMovesCalculator class to handle knight-specific "L" shaped movements

---
+++ shared/src/main/java/chess/MoveCalculators/KnightMovesCalculator.java
@@ -0,0 +1,17 @@
+package chess.MoveCalculators;
+
+public class KnightMovesCalculator extends PieceMovesCalculator {
+
+    @Override
+    protected int[][] getDirections() {
+        return new int[][]{
+                {2, 1}, {2, -1}, {-2, 1}, {-2, -1}, //various L-shaped moves
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+        };
+    }
+
+    @Override
+    protected int maxDistance() {
+        return 1;
+    }
+}
