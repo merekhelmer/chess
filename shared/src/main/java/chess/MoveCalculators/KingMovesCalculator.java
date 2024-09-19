065df948721b5bb9d0bf984a19c76bcb813defdc
Merek Helmer <merek.helmer@byu.edu>
2024-09-19 11:37:07 -0600

    Added KingMovesCalculator class to handle limited-range movement and direction

---
+++ shared/src/main/java/chess/MoveCalculators/KingMovesCalculator.java
@@ -0,0 +1,17 @@
+package chess.MoveCalculators;
+
+public class KingMovesCalculator extends PieceMovesCalculator {
+
+    @Override
+    protected int[][] getDirections() {
+        return new int[][]{
+                {1, 0}, {-1, 0}, {0, 1}, {0, -1},  //rook-like moves
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1} //bishop-like moves
+        };
+    }
+
+    @Override
+    protected int maxDistance() {
+        return 1;
+    }
+}
