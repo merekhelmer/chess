4fd96a2a7b4dd4420f078e64456146a7e3ef09ef
Merek Helmer <merek.helmer@byu.edu>
2024-09-19 11:27:01 -0600

    Implementation of BishopMovesCalculator and directions

---
+++ shared/src/main/java/chess/MoveCalculators/BishopMovesCalculator.java
@@ -0,0 +1,16 @@
+package chess.MoveCalculators;
+
+public class BishopMovesCalculator extends PieceMovesCalculator {
+
+    @Override
+    protected int[][] getDirections() {
+        return new int[][]{
+                {1, 1},
+                {-1, 1},
+                {-1, -1},
+                {1, -1}
+        };
+    }
+
+}
+
