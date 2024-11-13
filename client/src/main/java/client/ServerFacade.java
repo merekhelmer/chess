2e3b095dedf55a79ed2fa41818d75f029de5c8d9
Merek Helmer <merek.helmer@byu.edu>
2024-11-13 14:40:08 -0700

    ServerFacade methods for calling GAME related API endpoints (listGames, createGame, joinGame)

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,11 @@
 package client;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import model.*;
+import service.requests.CreateGameRequest;
+import service.requests.JoinGameRequest;
+import service.results.CreateGameResult;
 
 import java.io.*;
 import java.net.*;
@@ -17,7 +21,7 @@ public class ServerFacade {
         this.gson = new Gson();
     }
 
-    // User Operations
+    // USER
     public AuthData register(UserData userData) throws ResponseException {
         String path = "/user";
         return makeRequest("POST", path, userData, AuthData.class, null);
@@ -31,5 +35,26 @@ public class ServerFacade {
     public void logout(String authToken) throws ResponseException {
         String path = "/session";
         makeRequest("DELETE", path, null, null, authToken);
+    }
+
+    // GAME
+    public List<GameData> listGames(String authToken) throws ResponseException {
+        String path = "/game";
+        record ListGamesResponse(List<GameData> games) {
+        }
+        ListGamesResponse response = makeRequest("GET", path, null, ListGamesResponse.class, authToken);
+        return response.games();
+    }
+
+    public CreateGameResult createGame(String gameName, String authToken) throws ResponseException {
+        String path = "/game";
+        CreateGameRequest request = new CreateGameRequest(gameName);
+        return makeRequest("POST", path, request, CreateGameResult.class, authToken);
+    }
+
+    public void joinGame(int gameID, ChessGame.TeamColor playerColor, String authToken) throws ResponseException {
+        String path = "/game";
+        JoinGameRequest request = new JoinGameRequest(playerColor, gameID);
+        makeRequest("PUT", path, request, null, authToken);
     }
 }
