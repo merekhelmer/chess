924cff37550433e36ee1e0c4c9c4fff98b4acb90
Merek Helmer <merek.helmer@byu.edu>
2024-11-13 23:10:15 -0700

    Chess client main to initialize and manage login states with REPL interfaces - Reset `LoginState` to `SIGNEDOUT` and reinitialize for a new login session

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,38 @@
 import chess.*;
+import client.ServerFacade;
+import model.AuthData;
+import ui.LoginState;
+import ui.PostLoginREPL;
+import ui.PreLoginREPL;
+
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080"; // Adjust as necessary
+        ServerFacade serverFacade = new ServerFacade(serverUrl);
+        Scanner scanner = new Scanner(System.in);
+        AuthData authData = null;
+        LoginState state = LoginState.SIGNEDOUT;
+
+        while (true) {
+            if (state == LoginState.SIGNEDOUT) {
+                // Pre-login REPL
+                PreLoginREPL preLoginREPL = new PreLoginREPL(serverFacade, scanner);
+                authData = preLoginREPL.start();
+                if (authData != null) {
+                    state = LoginState.SIGNEDIN;
+                }
+            } else {
+                // Post-login REPL
+                PostLoginREPL postLoginREPL = new PostLoginREPL(serverFacade, scanner, authData);
+                boolean loggedOut = postLoginREPL.start();
+                if (loggedOut) {
+                    state = LoginState.SIGNEDOUT;
+                    authData = null;
+                }
+            }
+        }
     }
 }
