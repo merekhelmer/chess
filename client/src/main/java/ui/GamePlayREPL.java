364a69724c39b82bb50a2f99c6a6689724ead8d7
Merek Helmer <merek.helmer@byu.edu>
2024-12-01 15:10:16 -0700

    Refactor GamePlayREPL to use WebSocket connection -updated main gameplay loop to process user commands

--- client/src/main/java/ui/GamePlayREPL.java
+++ client/src/main/java/ui/GamePlayREPL.java
@@ -1,28 +1,59 @@
 package ui;
 
-import chess.ChessGame;
+import chess.*;
 import client.ResponseException;
 import client.ServerFacade;
 import model.AuthData;
+import websocket.WebSocketFacade;
+import websocket.messages.ServerMessage;
 
 import java.util.Scanner;
 
 public class GamePlayREPL {
 
-    private final ChessGame.TeamColor playerColor;
+    private final WebSocketFacade webSocketFacade;
+    private final Scanner scanner;
     private final ChessBoardRender boardRenderer;
+    private final ChessGame.TeamColor playerColor;
+    private final int gameID;
 
-    public GamePlayREPL(ServerFacade serverFacade, Scanner scanner, AuthData authData, int gameID,
-                        ChessGame.TeamColor playerColor) throws ResponseException {
+    public GamePlayREPL(WebSocketFacade webSocketFacade, Scanner scanner, AuthData authData,
+                        ChessGame.TeamColor playerColor, int gameID) {
+        this.webSocketFacade = webSocketFacade;
+        this.scanner = scanner;
+        this.boardRenderer = new ChessBoardRender(new ChessGame());
         this.playerColor = playerColor;
-
-        ChessGame chessGame = serverFacade.getGameState(gameID, authData.authToken());
-        this.boardRenderer = new ChessBoardRender(chessGame);
+        this.gameID = gameID;
     }
 
-    public void displayInitialGame() {
+    public void start() {
+        boolean inGame = true;
+
         boolean whiteAtBottom = playerColor == ChessGame.TeamColor.WHITE;
-        System.out.println("\nInitial board state:");
+        boardRenderer.renderBoard(whiteAtBottom, null);
+
+        while (inGame) {
+            System.out.println("\nEnter a command (help, redraw chess board, make move, resign, highlight " +
+                    "legal moves, leave):");
+            String command = scanner.nextLine().trim().toLowerCase();
+
+            switch (command) {
+                case "help" -> displayHelp();
+                case "redraw chess board" -> redrawChessBoard();
+                case "make move" -> makeMove();
+                case "resign" -> resign();
+                case "highlight legal moves" -> highlightLegalMoves();
+                case "leave" -> {
+                    leaveGame();
+                    inGame = false;
+                }
+                default -> System.out.println("Unknown command.");
+            }
+        }
+    }
+
+    public void redrawChessBoard() {
+        boolean whiteAtBottom = playerColor == ChessGame.TeamColor.WHITE;
         boardRenderer.renderBoard(whiteAtBottom, null);
     }
 }
