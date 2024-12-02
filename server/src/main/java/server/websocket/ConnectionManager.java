e42d8b5622e916078977d93a25d0103e309a41f3
Merek Helmer <merek.helmer@byu.edu>
2024-12-02 13:05:17 -0700

    ConnectionManager methods to add and remove WebSocket sessions by game ID, new broadcast method that sends messages to all open sessions using JSON format

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
@@ -10,4 +11,30 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
     private final ConcurrentHashMap<Integer, ArrayList<Session>> gameConnections = new ConcurrentHashMap<>();
+
+    public void add(Session session, int gameId) {
+        gameConnections.computeIfAbsent(gameId, k -> new ArrayList<>()).add(session);
+    }
+
+    public void remove(Session session) {
+        gameConnections.values().forEach(sessions -> sessions.remove(session));
+    }
+
+    public void broadcast(int gameID, ServerMessage message) throws IOException {
+        ArrayList<Session> sessions = gameConnections.get(gameID);
+        if (sessions != null) {
+
+            String jsonMessage = new Gson().toJson(message);
+            ArrayList<Session> toRemove = new ArrayList<>();
+
+            for (Session session : sessions) {
+                if (session.isOpen()) {
+                    session.getRemote().sendString(jsonMessage);
+                } else {
+                    toRemove.add(session);
+                }
+            }
+            sessions.removeAll(toRemove);
+        }
+    }
 }
