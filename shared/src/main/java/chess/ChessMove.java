1f51af3afcff3bd65a5bc437eedf089f3ce54b79
Merek Helmer <merek.helmer@byu.edu>
2024-12-05 11:35:07 -0700

    Minor if statement change for autograder CQ

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -27,7 +27,9 @@ public class ChessMove {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
